fdad408032b68deb6ea9a29c3ac27205fdcc3dea
Justin Movitz <movitzj1@montclair.edu>
2020-10-21 19:48:19 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -11,7 +11,7 @@ def fourth_challenge
    },
    capulet: {
       patriarch: {name: "Lord Capulet", age: "50"},
-      matriarch: => {name: "Lady Capulet", age: "51"},
+      matriarch: {name: "Lady Capulet", age: "51"},
       heroine: {name: "Juliet", age: "15", status: "alive"},
       heroine_friends: [
           {name: "Steven", age: "30", attitude: "confused"},
